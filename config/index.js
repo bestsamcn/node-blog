a58d0683a726ce9174457ed102245ef7f8d658e2
bestsamcn <645298225@qq.com>
2018-08-09 14:20:25 +0800

    redis

--- config/index.js
+++ config/index.js
@@ -2,15 +2,16 @@ var NODE_ENV = process.env.BLOG_ENV || 'development',
 	NODE_HOST = process.env.BLOG_HOST || '127.0.0.1',
 	NODE_PORT = process.env.BLOG_PORT || 3040,
 	TOKEN_SECRET = process.env.BLOG_SECRET || 'node-blog',
-	TOKEN_EXPIRES = process.env.TOKEN_EXPIRES || 1;
+	TOKEN_EXPIRES = process.env.TOKEN_EXPIRES || 1,
 	NODE_REDIS_PORT = process.env.NODE_REDIS_PORT || 6379,
+	NODE_REDIS_PASS = process.env.NODE_REDIS_PASS || '',
 	TEMPLATE_CACHE = ( NODE_ENV !== 'development' ),
 	MONGO_DATABASE = process.env.BLOG_MONGO_DATABASE || 'blog',
 	MONGO_USER = process.env.BLOG_MONGO_USER || '',
-	MONGO_PASSWORD = process.env.BLOG_MONGO_PASSWORD || '';
-	MONGO_PORT = process.env.BLOG_MONGO_PORT || 27017;
-	POST_LIMIT = process.env.BLOG_MONGO_PORT || '10mb';
-	EMAIL = process.env.BLOG_EMAIL || '';
+	MONGO_PASSWORD = process.env.BLOG_MONGO_PASSWORD || '',
+	MONGO_PORT = process.env.BLOG_MONGO_PORT || 27017,
+	POST_LIMIT = process.env.BLOG_POST_LIMIT || '10mb',
+	EMAIL = process.env.BLOG_EMAIL || '',
 	EMAIL_AUTH = process.env.BLOG_EMAIL_AUTH || '';
 	HOT_WORD_LENGTH = 10;
 var _config = {
@@ -28,6 +29,8 @@ var _config = {
 	POST_LIMIT:POST_LIMIT,
 	HOT_WORD_LENGTH:HOT_WORD_LENGTH,
 	EMAIL:EMAIL,
-	EMAIL_AUTH:EMAIL_AUTH
+	EMAIL_AUTH:EMAIL_AUTH,
+	NODE_REDIS_PORT:NODE_REDIS_PORT,
+	NODE_REDIS_PASS:NODE_REDIS_PASS
 }
 exports = module.exports = _config;
--- model/connect.js
+++ model/connect.js
@@ -2,7 +2,7 @@
  * connect
  * 数据库连接
  */
-var config = require('../config').mongoConfig;
+var config = require('../config');
 
 /**
  * 链接mongodb
@@ -12,7 +12,7 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.set('debug',true);
 
-var db = mongoose.connect(config.mongodb);
+var db = mongoose.connect(config.mongoConfig.mongodb);
 db.connection.on('error',function(error){
 	console.log('数据库连接失败'+error);
 });
@@ -26,8 +26,11 @@ exports.mongodb = db;
  * 链接redis
  */
 var redis = require('redis');
-var redisClient = redis.createClient(6379);
+var redisClient = redis.createClient(config.NODE_REDIS_PORT);
 
+redisClient.auth(config.NODE_REDIS_PASS, function(){
+	console.log('auth sucess');
+});
 redisClient.on('error', function (err) {
     console.log('Error ' + err);
 });
